5d1b0ad4520affa46351f207e8e08df5721953aa
Anumol K Mani <anumol.km32@gmail.com>
2022-08-05 18:11:07 +0400

    commit

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -40,7 +40,7 @@ const SocialLinks = () => {
             Resume <BsFillPersonLinesFill size={30} />
           </>
         ),
-        href: "/Anu CV - 05.08.2022.pdf",
+        href: "/Anu CV -frontend.pdf",
         style: "rounded-br-md",
         download: true,
       },
